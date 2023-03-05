25513aeaf7f87c3e01e1626352f7add167b691e8
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-03-05 11:47:13 +0300

    написан код программы для решения проверочной работы

--- final_task/Program.cs
+++ final_task/Program.cs
@@ -8,3 +8,68 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
+Console.Clear();
+// Метод для ввода массива строк от пользователя
+string[] UserInputArray(int num)
+{
+    string[] array = new string[num];
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"Введите строку №{i}: ");
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+}
+
+// Метод для создания нового массива из 3 и меньше символов
+string[] ThreeCharArray(string[] userArray)
+{
+    int count = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i].Length <= 3)
+            count++;
+    }
+    string[] finishArray = new string[count];
+    count = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i].Length <= 3)
+        {
+            finishArray[count] = userArray[i];
+            count++;
+        }
+    }    
+    return finishArray;
+}
+
+// Метод печати массива строк в консоль
+void PrintArray(string[] printArr)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < printArr.Length; i++)
+    {
+        if (i == printArr.Length - 1)
+            System.Console.Write($"\"{printArr[i]}\"");
+        else
+            System.Console.Write($"\"{printArr[i]}\", ");
+    }
+    System.Console.WriteLine("]");
+}
+
+System.Console.WriteLine("Запрос от пользователя массив строк.\n"
+                + "Вывод всех строк состоящих из 3 и меньше символов в новый массив.");
+System.Console.WriteLine("Сколько будет строк в исходном массиве?");
+System.Console.Write(">>: ");
+int.TryParse(Console.ReadLine(), out int num);
+while (num <= 0)
+{
+    System.Console.WriteLine("Число меньше 1! Невозможно создать массив. Введите заново: ");
+    int.TryParse(Console.ReadLine(), out num);
+}
+string[] userArray = UserInputArray(num);
+string[] threeCharArray = ThreeCharArray(userArray);
+System.Console.WriteLine("Изначальный массив:");
+PrintArray(userArray);
+System.Console.WriteLine("Полученный массив:");
+PrintArray(threeCharArray);
